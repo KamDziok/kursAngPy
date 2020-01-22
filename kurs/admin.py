4fe5cb50cd9fc77c96ffc55c1f738c09bd25cb19
KamDziok <kam_dziok@wp.pl>
2020-01-22 22:40:37 +0100

    wczytywanie testów z bazy

--- kurs/admin.py
+++ kurs/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from accounts.models import TestyUzytkownikow, Pytania, Test
-
-admin.site.register(Test)
-admin.site.register(TestyUzytkownikow)
-admin.site.register(Pytania)
-
 # Register your models here.
