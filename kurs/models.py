228deabfe1a0aaf120cdd10490f1dc7ba6ecbbe8
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-01-23 13:02:02 +0100

    dodanie modeli i widokow

--- kurs/models.py
+++ kurs/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Kategorie(models.Model):
+    kategoria = models.CharField('Kategoria', max_length=100)
+
+    # class Meta:
+    #     verbose_name = "Kategoria"
+    #     verbose_name_plural = "Kategorie"
+
+    def __unicode__(self):
+        return self.kategoria
+
+
+class Dzial(models.Model):
+    kategoria = models.ForeignKey(Kategorie, on_delete=models.CASCADE)
+    dzial = models.CharField('Temat', max_length=100)
+
+    # class Meta:
+    #     verbose_name = "Dzial"
+    #     verbose_name_plural = "Dzialy"
+
+    def __unicode__(self):
+        return self.dzial
+
+
+class Temat(models.Model):
+    dzial = models.ForeignKey(Dzial, on_delete=models.CASCADE)
+    temat = models.CharField('Temat', max_length=100)
+    plik = models.CharField('Plik', max_length=100)
+
+    # class Meta:
+    #     verbose_name = "Temat"
+    #     verbose_name_plural = "Tematy"
+
+    def __unicode__(self):
+        return self.temat
--- kurs/views.py
+++ kurs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Kategorie, Dzial, Temat
 
 # Create your views here.
+
+
+def renderMenu(request):
+    menu = Kategorie.kategoria
+    return render(request, 'templates/base.html', {'menu': menu})
+
+
+def renderDzial(request):
+    dzial = Dzial.dzial
+    return render(request, 'templates/base.html', {'dzial': dzial})
+
+
+def renderTemat(request):
+    temat = Temat.temat
+    return render(request, 'templates/base.html', {'temat': temat})
